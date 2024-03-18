6e192b4190be6a82e6f74908fa9940d88e7fd93d
Edwin Tang <edwintang10@gmail.com>
2024-03-18 18:02:23 +0800

    feat(config): add structs for json schema

--- config/config.go
+++ config/config.go
@@ -7,7 +7,33 @@ import (
 	"os"
 )
 
+type Measurement int
+
+const (
+	ResponseTime Measurement = iota
+	BlockingDuration
+)
+
+type TestType int
+
+const (
+	Sequence TestType = iota
+	Parallel
+)
+
 type Step struct {
+	Name      string      `json:"name"`
+	Endpoint  string      `json:"endpoint"`
+	TestType  TestType    `json:"testType"`
+	Load      int         `json:"load"`
+	DependsOn []string    `json:"dependsOn"`
+	Measure   Measurement `json:"measure"`
+}
+
+type Config struct {
+	Version int    `json:"version"`
+	URL     string `json:"url"`
+	Steps   []Step `json:"steps"`
 }
 
 func readConfig() ([]Step, error) {
