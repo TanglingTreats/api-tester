1ddd684afc8a8ff1422a0d5340b308d2f4eb5b8c
Edwin Tang <edwintang10@gmail.com>
2024-03-21 15:34:40 +0800

    refactor(enum): update measurement enum.

--- config/configenums.go
+++ config/configenums.go
@@ -1,18 +1,104 @@
 package config
 
-type StepType string
-
-const (
-	Sequential StepType = "sequential"
-	Concurrent StepType = "concurrent"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
 )
 
-type Measurement string
+// --- Step Type enum ---
+type StepType int
 
 const (
-	ResponseTime     Measurement = "response time"
-	BlockingDuration Measurement = "blocking duration"
+	Sequential StepType = iota + 1
+	Concurrent
 )
+
+var (
+	StepTypeName = map[StepType]string{
+		Sequential: "sequential", Concurrent: "concurrent",
+	}
+	StepTypeValue = map[string]StepType{
+		"sequential": Sequential, "concurrent": Concurrent,
+	}
+)
+
+func parseStepType(s string) (StepType, error) {
+	s = strings.TrimSpace(strings.ToLower(s))
+	value, ok := StepTypeValue[s]
+	if !ok {
+		return StepType(0), fmt.Errorf("%q is not a valid step type", s)
+	}
+	return StepType(value), nil
+}
+
+func (st StepType) String() string {
+	return StepTypeName[st]
+}
+
+func (st StepType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(st.String())
+}
+
+func (st *StepType) UnmarshalJSON(data []byte) (err error) {
+	var stepType string
+	if err := json.Unmarshal(data, &stepType); err != nil {
+		return err
+	}
+	if *st, err = parseStepType(stepType); err != nil {
+		return err
+	}
+	return nil
+}
+
+// --- --- ---
+// --- Measurement ---
+type Measurement int
+
+const (
+	ResponseTime Measurement = iota
+	BlockingDuration
+)
+
+var (
+	MeasurementName = map[Measurement]string{
+		ResponseTime: "response time", BlockingDuration: "blocking duration",
+	}
+
+	MeasurementValue = map[string]Measurement{
+		"response time": ResponseTime, "blocking duration": BlockingDuration,
+	}
+)
+
+func parseMeasurement(s string) (Measurement, error) {
+	s = strings.TrimSpace(strings.ToLower(s))
+	value, ok := StepTypeValue[s]
+	if !ok {
+		return Measurement(0), fmt.Errorf("%q is not a valid step type", s)
+	}
+	return Measurement(value), nil
+}
+
+func (m Measurement) String() string {
+	return MeasurementName[m]
+}
+
+func (m Measurement) MarshalJSON() ([]byte, error) {
+	return json.Marshal(m.String())
+}
+
+func (m *Measurement) UnmarshalJSON(data []byte) (err error) {
+	var measurement string
+	if err := json.Unmarshal(data, &measurement); err != nil {
+		return err
+	}
+	if *m, err = parseMeasurement(measurement); err != nil {
+		return err
+	}
+	return nil
+}
+
+// --- --- ---
 
 type TestType string
 
